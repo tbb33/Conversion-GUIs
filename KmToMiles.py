f58194afbefd434cd7bab961c163ba1321010f0c
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-11-02 13:26:30 -0500

    Update KmToMiles.py
    Adding calc to convert km to miles
    Adding labels

--- KmToMiles.py
+++ KmToMiles.py
@@ -3,9 +3,8 @@ from tkinter import *
 
 window=Tk()
 def km_to_miles():
-    print(e1_value.get())
-    #END enter new text at bottom existing text if many lines
-    t1.insert(END, e1_value.get())
+    miles=float(e1_value.get())*.6213
+    t1.insert(END, miles)
 
 b1=Button(window, text = "Execute", command=km_to_miles)
 b1.grid(row=0,column=0)
@@ -14,6 +13,13 @@ e1_value = StringVar()
 e1 = Entry(window, textvariable = e1_value)
 e1.grid(row=0,column=1)
 
+lb1 = Label(window, text="km")
+lb1.grid(row=0,column=2)
+
 t1 = Text(window, height=1, width=20)
-t1.grid(row=0,column=2)
+t1.grid(row=0,column=3)
+
+lb2 = Label(window, text="mi")
+lb2.grid(row=0,column=4)
+
 window.mainloop()
