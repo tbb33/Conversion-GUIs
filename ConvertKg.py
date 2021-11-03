1e4fd0e85846593a01517ea4d0b9baa9a775d8af
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-11-02 21:13:57 -0500

    Adding function to calc conversions
    Adding convert function
    Adding command to button widget to call convert function
    Adding comments

--- ConvertKg.py
+++ ConvertKg.py
@@ -3,29 +3,40 @@ from tkinter import *
 
 window=Tk()
 window.title("Converting Kilograms")
+def convert():
+    grams = float(e1_value.get())*1000
+    t1.insert(END, grams)
+    pounds = float(e1_value.get())*2.20462
+    t2.insert(END, pounds)
+    ounce = float(e1_value.get())*35.274
+    t3.insert(END, ounce)
 
-kglabel =Label(window, text="Kg")
-kglabel.grid(row=0,column=3)
-b1=Button(window, text = "Convert")
+lb =Label(window, text="Kg")
+lb.grid(row=0,column=3)
+#when button "Convert" is pushed, the convert func is called
+b1=Button(window, text = "Convert", command=convert)
 b1.grid(row=0,column=4)
 
-kg_value = StringVar()
-kg = Entry(window, textvariable = kg_value)
-kg.grid(row=0,column=2)
+e1_value = StringVar() #special stringvar object
+#entry box to capture user input
+e1 = Entry(window, textvariable = e1_value)
+e1.grid(row=0,column=2)
 
-g = Text(window, height=1, width=20)
-g.grid(row=1 ,column=0)
-glabel =Label(window, text="g")
-glabel.grid(row=1,column=1)
+#three text boxes with respective labels next to them
+t1 = Text(window, height=1, width=20)
+t1.grid(row=1 ,column=0)
+lb1 =Label(window, text="g")
+lb1.grid(row=1,column=1)
 
-lb = Text(window, height=1, width=20)
-lb.grid(row=1, column=2)
-lblabel =Label(window, text="lb")
-lblabel.grid(row=1,column=3)
+t2 = Text(window, height=1, width=20)
+t2.grid(row=1, column=2)
+lb2 =Label(window, text="lb")
+lb2.grid(row=1,column=3)
 
-oz = Text(window, height=1, width=20)
-oz.grid(row=1, column=4)
-ozlabel =Label(window, text="oz")
-ozlabel.grid(row=1,column=5)
+t3 = Text(window, height=1, width=20)
+t3.grid(row=1, column=4)
+lb3 =Label(window, text="oz")
+lb3.grid(row=1,column=5)
 
+#keeps window open until user closes it
 window.mainloop()
