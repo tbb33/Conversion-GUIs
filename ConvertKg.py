bd7d6e7feeb96dc47a1fca078500e64241193137
tbb33 <91215510+tbb33@users.noreply.github.com>
2021-11-02 21:20:50 -0500

    Function to calculate conversions
    Add convert function
    Add command to button to call convert function
    Add comments

--- ConvertKg.py
+++ ConvertKg.py
@@ -3,16 +3,26 @@ from tkinter import *
 
 window=Tk()
 window.title("Converting Kilograms")
+def convert():
+    grams = float(kg_value.get())*1000
+    g.insert(END, grams)
+    pounds = float(kg_value.get())*2.20462
+    lb.insert(END, pounds)
+    ounce = float(kg_value.get())*35.274
+    oz.insert(END, ounce)
 
 kglabel =Label(window, text="Kg")
 kglabel.grid(row=0,column=3)
-b1=Button(window, text = "Convert")
+#when button "Convert" is pushed, the convert func is called
+b1=Button(window, text = "Convert", command=convert)
 b1.grid(row=0,column=4)
 
-kg_value = StringVar()
+kg_value = StringVar() #special stringvar object
+#entry box to capture user input
 kg = Entry(window, textvariable = kg_value)
 kg.grid(row=0,column=2)
 
+#three text boxes with respective labels next to them
 g = Text(window, height=1, width=20)
 g.grid(row=1 ,column=0)
 glabel =Label(window, text="g")
@@ -28,4 +38,5 @@ oz.grid(row=1, column=4)
 ozlabel =Label(window, text="oz")
 ozlabel.grid(row=1,column=5)
 
+#keeps window open until user closes it
 window.mainloop()
